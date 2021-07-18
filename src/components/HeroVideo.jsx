06424a17e57b9661b473b3eb638bd4928e86f084
Arnab Biswas <khanp4397@gmail.com>
2021-07-18 22:44:31 +0530

    Fixing HeroVideo VideoURL

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -2,7 +2,7 @@ function HeroVideo({ videoURL }) {
     return (
         <div className='heroCard'>
             {/* <video src={videoURL} autoPlay={true} loop muted allow="autoplay"></video> */}
-            <iframe style={{width: '100%', height: '70vh'}} src="https://www.youtube.com/embed/i2p_742MOO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
+            <iframe style={{width: '100%', height: '70vh'}} src={videoURL} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
         mozallowfullscreen="mozallowfullscreen" 
         msallowfullscreen="msallowfullscreen" 
         oallowfullscreen="oallowfullscreen" 
