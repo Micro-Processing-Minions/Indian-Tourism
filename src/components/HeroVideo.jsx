e1a26dc36da6d9289ea7ea18f81aa93ddbd8de7d
Arnab Biswas <khanp4397@gmail.com>
2021-07-18 22:42:39 +0530

    Reuploading

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -2,7 +2,11 @@ function HeroVideo({ videoURL }) {
     return (
         <div className='heroCard'>
             {/* <video src={videoURL} autoPlay={true} loop muted allow="autoplay"></video> */}
-            <iframe style={{width: '100%', height: '70vh'}} src="https://www.youtube.com/embed/i2p_742MOO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe style={{width: '100%', height: '70vh'}} src="https://www.youtube.com/embed/i2p_742MOO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"
+        mozallowfullscreen="mozallowfullscreen" 
+        msallowfullscreen="msallowfullscreen" 
+        oallowfullscreen="oallowfullscreen" 
+        webkitallowfullscreen="webkitallowfullscreen"></iframe>
         </div>
     )
 }
