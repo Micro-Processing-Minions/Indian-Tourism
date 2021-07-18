2edac71c23fbcb6ba1846b206064249391998c7d
Arnab Biswas <khanp4397@gmail.com>
2021-07-18 20:28:26 +0530

    Building Build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ["/src/index.css", './src/**.js', './src/**.js', './src/components/**.jsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
