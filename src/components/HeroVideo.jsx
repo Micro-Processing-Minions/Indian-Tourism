646917df6a3a4df356839ce072bde70825a52077
Arnab Biswas <khanp4397@gmail.com>
2021-07-18 20:40:37 +0530

    fixing Audio Problem

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,7 +1,7 @@
 function HeroVideo({ videoURL }) {
     return (
         <div className='heroCard'>
-            <video src={videoURL} autoPlay loop muted></video>
+            <video src={videoURL} autoPlay={true} loop muted allow="autoplay"></video>
         </div>
     )
 }
