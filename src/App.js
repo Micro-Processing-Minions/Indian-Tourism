28f032ae624249000a1d4bf0ce027b961affd474
Arnab Biswas <khanp4397@gmail.com>
2021-07-19 10:03:06 +0530

    Conflicts Fixed and Added Location Page to Correct Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-import Navbar from "./components/Navbar";
-import CardBody from "./components/CardBody";
-=======
 import Navbar from "./components/Navbar"
 import MainBody from "./components/MainBody"
 import Location from './components/Location'
+import CardBody from "./components/CardBody";
 import { BrowserRouter, Switch, Route, useParams } from 'react-router-dom'
 function HandleLocation(){
   let { loc } = useParams();
@@ -13,28 +10,22 @@ function HandleLocation(){
   )
 }
 
->>>>>>> ef94d7ae37b1427b54572d75cbad8c642016dbd6
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      <CardBody />
-=======
       <BrowserRouter basename='/Indian-Tourism/build'>
         <Navbar />
-
-
+        
         <Switch>
           <Route path='/' exact>
             <MainBody />
           </Route>
           <Route path='/:loc' children={<HandleLocation />}>
+          <CardBody />
           </Route>
         </Switch>
       </BrowserRouter>
->>>>>>> ef94d7ae37b1427b54572d75cbad8c642016dbd6
     </div>
   );
 }
